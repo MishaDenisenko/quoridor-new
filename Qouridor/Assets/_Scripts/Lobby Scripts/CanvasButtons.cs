e625eba4cd46f41011508f0e8587de58d15197dc
Misha <midenisenko@gmail.com>
2022-01-12 00:38:17 +0200

    add main scene

---
+++ Qouridor/Assets/_Scripts/Lobby Scripts/CanvasButtons.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasButtons : MonoBehaviour {
+    private Transform _buttonTransform;
+    
+    private void Awake() {
+        _buttonTransform = transform;
+    }
+
+    public void PlayGame() {
+        StartCoroutine(LoadScene("GameScene"));
+    }
+    
+    public void OpenHomeScene() {
+        StartCoroutine(LoadScene("Lobby"));
+    }
+    
+    public void SetPressedButton() {
+        _buttonTransform.Rotate(0, 0, -5);
+        if (SceneManager.GetActiveScene().name.Equals("Lobby")) _buttonTransform.localScale = new Vector3(0.8f, 0.8f, 1f);
+        else if (SceneManager.GetActiveScene().name.Equals("GameScene"))
+            _buttonTransform.localScale = new Vector3(0.6f, 0.6f, 1f);
+    }
+
+    public void SetDefaultButton() {
+        _buttonTransform.Rotate(0, 0, 5);
+        if (SceneManager.GetActiveScene().name.Equals("Lobby")) _buttonTransform.localScale = new Vector3(1f, 1f, 1f);
+        else if (SceneManager.GetActiveScene().name.Equals("GameScene"))
+            _buttonTransform.localScale = new Vector3(0.8f, 0.8f, 1f);
+    }
+
+    IEnumerator LoadScene(string sceneName) {
+        float fadeTime = Camera.main.GetComponent<Fading>().Fade(1);
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Qouridor/Assets/_Scripts/Lobby Scripts/Fading.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fading : MonoBehaviour {
+
+    public Texture2D fading;
+    private float _fadeSpeed = 0.8f;
+    private int _drawDepth = -1000;
+    public float alpha = 1f;
+    private sbyte _fadeDir = -1;
+
+    private void OnGUI() {
+        alpha += _fadeDir * _fadeSpeed * Time.deltaTime;
+        alpha = Mathf.Clamp01(alpha);
+
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+        GUI.depth = _drawDepth;
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fading);
+    }
+
+    public float Fade(sbyte dir) {
+        _fadeDir = dir;
+        return _fadeSpeed;
+    }
+}
